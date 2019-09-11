6a821cbc50c421ea587ed18f78bcf30b5171d2a4
samfultonschool <samfultonschool@gmail.com>
2019-09-10 17:50:33 -0700

    regen color corrected

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 
-
+// @red: rgb(255, 0, 0);
+// @red-violet: rgb(255, 0, 149);
+// @violet: rgb(172, 0, 252);
+// @blue-violet: rgb(109, 0, 252);
+// @blue: rgb(0, 0, 255);
+// @blue-green: rgb(0, 183, 255);
+// @green: green;
+// @yellow-green: rgb(145, 255, 0);
+// @yellow: rgb(255, 255, 0);
+// @yellow-orange: rgb(255, 196, 0);
+// @orange: rgb(253, 169, 0);
+// @red-orange: rgb(255, 72, 0);
 
 
 async function newRandomColors(){
-    await changeColor({name:'red', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    await changeColor({name:'red-violet', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:100});
-    await changeColor({name:'violet', rMin:100, rMax:255, gMin:0, gMax:50, bMin:50, bMax:200});
-    await changeColor({name:'blue-violet', rMin:25, rMax:100, gMin:0, gMax:50, bMin:100, bMax:255});
-    await changeColor({name:'blue', rMin:0, rMax:50, gMin:0, gMax:50, bMin:100, bMax:255});
-    await changeColor({name:'blue-green', rMin:0, rMax:0, gMin:50, gMax:250, bMin:50, bMax:250});
-    await changeColor({name:'green', rMin:100, rMax:255, gMin:0, gMax:250, bMin:0, bMax:50});
-    await changeColor({name:'yellow-green', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    await changeColor({name:'yellow', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    await changeColor({name:'yellow-orange', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    await changeColor({name:'orange', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    await changeColor({name:'red-orange', rMin:100, rMax:255, gMin:0, gMax:20, bMin:0, bMax:20});
+    await changeColor({name:'red', rMin:200, rMax:255, gMin:0, gMax:25, bMin:0, bMax:25});
+    await changeColor({name:'red-violet', rMin:100, rMax:255, gMin:0, gMax:25, bMin:50, bMax:150});
+    await changeColor({name:'violet', rMin:100, rMax:255, gMin:0, gMax:25, bMin:50, bMax:250});
+    await changeColor({name:'blue-violet', rMin:50, rMax:200, gMin:0, gMax:25, bMin:150, bMax:220});
+    await changeColor({name:'blue', rMin:0, rMax:50, gMin:0, gMax:50, bMin:150, bMax:255});
+    await changeColor({name:'blue-green', rMin:0, rMax:25, gMin:150, gMax:250, bMin:150, bMax:250});
+    await changeColor({name:'green', rMin:0, rMax:25, gMin:200, gMax:250, bMin:0, bMax:50});
+    await changeColor({name:'yellow-green', rMin:50, rMax:200, gMin:150, gMax:250, bMin:0, bMax:25});
+    await changeColor({name:'yellow', rMin:150, rMax:255, gMin:150, gMax:255, bMin:0, bMax:25});
+    await changeColor({name:'yellow-orange', rMin:200, rMax:255, gMin:100, gMax:250, bMin:0, bMax:50});
+    await changeColor({name:'orange', rMin:200, rMax:255, gMin:50, gMax:150, bMin:0, bMax:25});
+    await changeColor({name:'red-orange', rMin:200, rMax:255, gMin:25, gMax:100, bMin:0, bMax:25});
     
 }
 
