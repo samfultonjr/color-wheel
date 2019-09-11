8a3c8cd258d1842bb97d4e0431839943f5e8c44f
samfultonschool <samfultonschool@gmail.com>
2019-09-10 17:21:48 -0700

    regen

--- script.js
+++ script.js
@@ -2,32 +2,30 @@
 
 
 
-function newRandomColors(){
-    changeColor({name:'@red', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    changeColor({name:'@red-violet', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:100});
-    changeColor({name:'@violet', rMin:100, rMax:255, gMin:0, gMax:50, bMin:50, bMax:200});
-    changeColor({name:'@blue-violet', rMin:25, rMax:100, gMin:0, gMax:50, bMin:100, bMax:255});
-    changeColor({name:'@blue', rMin:0, rMax:50, gMin:0, gMax:50, bMin:100, bMax:255});
-    changeColor({name:'@blue-green', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    changeColor({name:'@green', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    changeColor({name:'@yellow-green', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    changeColor({name:'@yellow', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    changeColor({name:'@yellow-orange', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    changeColor({name:'@orange', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    changeColor({name:'@red-orange', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
-    less.refreshStyles();
+async function newRandomColors(){
+    await changeColor({name:'red', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
+    await changeColor({name:'red-violet', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:100});
+    await changeColor({name:'violet', rMin:100, rMax:255, gMin:0, gMax:50, bMin:50, bMax:200});
+    await changeColor({name:'blue-violet', rMin:25, rMax:100, gMin:0, gMax:50, bMin:100, bMax:255});
+    await changeColor({name:'blue', rMin:0, rMax:50, gMin:0, gMax:50, bMin:100, bMax:255});
+    await changeColor({name:'blue-green', rMin:0, rMax:0, gMin:50, gMax:250, bMin:50, bMax:250});
+    await changeColor({name:'green', rMin:100, rMax:255, gMin:0, gMax:250, bMin:0, bMax:50});
+    await changeColor({name:'yellow-green', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
+    await changeColor({name:'yellow', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
+    await changeColor({name:'yellow-orange', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
+    await changeColor({name:'orange', rMin:100, rMax:255, gMin:0, gMax:50, bMin:0, bMax:50});
+    await changeColor({name:'red-orange', rMin:100, rMax:255, gMin:0, gMax:20, bMin:0, bMax:20});
+    
 }
 
-newRandomColors();
+
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-function changeColor({name, rMin, rMax, gMin, gMax, bMin, bMax}){
-    let mods = {};
-    mods[name] = `rgb(${random(rMin, rMax)}, ${random(gMin, gMax)}, ${random(bMin, bMax)})`
-    less.modifyVars(mods);
+async function changeColor({name, rMin, rMax, gMin, gMax, bMin, bMax}){
+    document.getElementById(name).style.borderColor  = `rgb(${random(rMin, rMax)}, ${random(gMin, gMax)}, ${random(bMin, bMax)}) transparent transparent transparent`;
 }
 
 
@@ -36,3 +34,10 @@ function changeColor({name, rMin, rMax, gMin, gMax, bMin, bMax}){
 document.querySelector('.regenerate').addEventListener('click', ()=>{
     newRandomColors();
 })
+
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
